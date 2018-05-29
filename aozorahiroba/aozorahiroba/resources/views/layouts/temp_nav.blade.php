e9e36b350b074583c59f2db5922ee4280a00aab1
ys <thinkingradish@Gmail.com>
2018-05-29 13:53:05 +0900

    Addtopnav

---
+++ aozorahiroba/aozorahiroba/resources/views/layouts/temp_nav.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+	integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
+	crossorigin="anonymous">
+<title></title>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light"
+		style="background-color: #02cbd6">
+		<a class="navbar-brand" href="#"
+			style="color: white; font-size: 28px;">青空広場</a>
+		<button type="button" class="navbar-toggler" data-toggle="collapse"
+			data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+			aria-expanded="false" aria-label="ナビゲーションの切替">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav ml-auto">
+				<a class="nav-item nav-link" href="#"
+					style="color: white; margin: 0 15px 0 10px; font-size: 18px;" onmouseover="this.style.color='#d3f911'" onmouseout="this.style.color='white'">新規利用者登録</a>
+				<a class="nav-item nav-link" href="#"
+					style="color: white; margin: 0 15px 0 10px; font-size: 18px;" onmouseover="this.style.color='#d3f911'" onmouseout="this.style.color='white'">自身の利用者情報を編集する</a>
+				<a class="nav-item nav-link" href="#"
+					style="color: white; margin: 0 15px 0 10px; font-size: 18px;" onmouseover="this.style.color='#d3f911'" onmouseout="this.style.color='white'">利用者を探す</a>
+				<a class="nav-link" href="#"
+					style="color: white; margin: 0 15px 0 10px; font-size: 18px;" onmouseover="this.style.color='#d3f911'" onmouseout="this.style.color='white'">利用者認証</a>
+			</div>
+		</div>
+	</nav>
+	<div>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+			integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+			crossorigin="anonymous"></script>
+		<script
+			src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+			integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+			crossorigin="anonymous"></script>
+		<script
+			src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+			integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+			crossorigin="anonymous"></script>
+	</div>
+</body>
+</html>
